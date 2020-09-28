e70a259792424c239a183254592318e59edf5fac
Paid_co <hellopaidco@gmail.com>
2020-09-28 13:41:16 -0700

    updates

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,82 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import CardColumns from 'react-bootstrap/CardColumns';
+import Card from 'react-bootstrap/Card';
 
-export default function Home() {
+import CoinGecko from 'coingecko-api';
+const coinGeckoClient = new CoinGecko();
+
+export default function Home(props) {
+  const { data } = props.result;
+
+  const formatPercent = (number) => `${new Number(number).toFixed(2)}%`;
+
+  const formatDollar = (number, maximumSignificantDigits) =>
+    new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      maximumSignificantDigits,
+    }).format(number);
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Crypto Tiles</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+      <CardColumns className='mt-2 mr-2 ml-2'>
+        {data.map((coin) => (
+          <Card
+            bg={coin.price_change_percentage_24h > 0 ? 'success' : 'danger'}
+            text='white'
+            className='text-center p-2'
+            key={coin.id}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <blockquote className=' mb-0 card-body'>
+              <img src={coin.image} style={{ width: 50, height: 50 }} />
+              <h1 className='mt-2'>
+                {coin.symbol.toUpperCase()}{' '}
+                {formatDollar(coin.current_price, 20)}{' '}
+              </h1>
+              <h3>
+                {coin.price_change_percentage_24h > 0 ? '+' : ''}
+                {formatPercent(coin.price_change_percentage_24h)}
+              </h3>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+              <small className='text-muted'>
+                mkt cap {formatDollar(coin.market_cap, 12)}
+              </small>
+            </blockquote>
+          </Card>
+        ))}
+      </CardColumns>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps(context) {
+  const params = {
+    vs_currency: 'usd',
+    ids: [
+      'bitcoin',
+      'ethereum',
+      'polkadot',
+      'bitcoin-cash',
+      'basic-attention-token',
+      'cosmos',
+      'uniswap',
+      'icon',
+      'kyber-network',
+      'nervos-network',
+      'terra-luna',
+      'crypto-com-chain',
+    ],
+    order: 'volume_desc',
+  };
+  const result = await coinGeckoClient.coins.markets(params);
+  return {
+    props: {
+      result,
+    },
+  };
 }
